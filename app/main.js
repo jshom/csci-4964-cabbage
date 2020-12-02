fce223d9615ec714b6f25deefbf3c85f68da3add
MadSnailDisease <madsnaildisease123@gmail.com>
2020-12-02 17:18:03 -0500

    Makes base electron application

---
+++ app/main.js
@@ -0,0 +1,30 @@
+console.log("yo");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+process.env.ELECTRON_ENABLE_LOGGING = 1;
+
+const createWindow = () => {
+
+  mainWindow = new BrowserWindow({
+    width: 2000,
+    height: 1500,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.maximize();
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+app.allowRendererProcessReuse = true;
+
+app.on('ready', createWindow);
