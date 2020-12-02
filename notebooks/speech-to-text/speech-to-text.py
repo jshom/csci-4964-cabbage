be5d55cce8a603122349cb47d8bd1e37f7e78961
sen-francis <s3nfrancis@gmail.com>
2020-12-01 19:14:49 -0600

    Basic speech to text working

---
+++ notebooks/speech-to-text/speech-to-text.py
@@ -0,0 +1,30 @@
+import json
+from os.path import join, dirname
+#ibm_watson is not a default python module, install with: pip3 install ibm_watson
+from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+#assumes local_file_path is the the path of the audio to transcribe
+def speech_to_text(filename):
+    #Should probably remove API Key bc sensitive information
+    authenticator = IAMAuthenticator('KuyElaYRmMz5Sb4okniJfkAdryuTnjh_QtV0SfC5lwhG')
+    service = SpeechToTextV1(authenticator = authenticator)
+    #Should probably remove API URL bc sensitive information
+    service.set_service_url('https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/ac3e0c60-51d5-4b01-9acb-711c614ae767')
+    with open(join(dirname('__file__'),filename),'rb') as audio_file:
+        dic = json.loads(
+                json.dumps(
+                    service.recognize(
+                        audio=audio_file,
+                        content_type='audio/mp3',
+                        model='en-US_NarrowbandModel',
+                    continuous=True).get_result(), indent=2))
+    # Stores the transcribed text
+    str = ""
+    while bool(dic.get('results')):
+        str = dic.get('results').pop().get('alternatives').pop().get('transcript')+str[:]
+    print(str)
+
+#call function here
+speech_to_text('malcom-x-trim.mp3')
