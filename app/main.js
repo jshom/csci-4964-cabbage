2912af852e73793d0038725600934dd71f483183
Jacob Shomstein <shomsj@rpi.edu>
2020-12-07 18:08:28 -0500

    added templating for the audio page

--- app/main.js
+++ app/main.js
@@ -1,31 +1,41 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const ejs = require('ejs')
 
 let mainWindow
 
 process.env.ELECTRON_ENABLE_LOGGING = 1
 
+const BOX_EDGE_LENGTH = 600
+
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 2000,
-    height: 1500,
+    width: BOX_EDGE_LENGTH,
+    height: BOX_EDGE_LENGTH,
     webPreferences: {
       nodeIntegration: true
     }
   })
-
-  mainWindow.maximize()
-
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 }
 
-ipcMain.on("form-submit", function(event, channel, time){
-    console.log(channel, time);
-});
+ipcMain.on('form-submit', (_, channel, time) => {
+  const templateData = {
+    channel: channel,
+    time: time,
+    audioURL: '__placeholder__'
+  }
+  ejs.renderFile('./audio.ejs', templateData, (err, htmlString) => {
+    if (err) {
+      console.error(err)
+      htmlString = err
+    }
+    mainWindow
+      .loadURL(
+        'data:text/html;charset=utf-8,' +
+        encodeURI(htmlString))
+  })
+})
 
 app.allowRendererProcessReuse = true
 
