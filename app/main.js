a24d4fff9d780cf082cd1e534ae68041aee1e37a
Jacob Shomstein <shomsj@rpi.edu>
2020-12-07 19:35:53 -0500

    front end audio in

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const ejs = require('ejs')
+const pyshell = require('python-shell')
+const fs = require('fs')
 
 let mainWindow
 
@@ -13,27 +15,35 @@ const createWindow = () => {
     width: BOX_EDGE_LENGTH,
     height: BOX_EDGE_LENGTH,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      webSecurity: false
     }
   })
   mainWindow.loadFile(path.join(__dirname, 'index.html'))
+  mainWindow.webContents.openDevTools()
 }
 
 ipcMain.on('form-submit', (_, channel, time) => {
-  const templateData = {
-    channel: channel,
-    time: time,
-    audioURL: '__placeholder__'
-  }
-  ejs.renderFile('./audio.ejs', templateData, (err, htmlString) => {
-    if (err) {
-      console.error(err)
-      htmlString = err
+  const text = 'thing to turn to voice'
+
+  pyshell.PythonShell.run('textToSpeech.py', { args: [text] }, (err, _) => {
+    if (err) throw err
+    const audioURL = 'data:audio/flac;base64,' + fs.readFileSync('./audio_file.flac').toString('base64')
+    const templateData = {
+      channel: channel,
+      time: time,
+      audioURL: audioURL
     }
-    mainWindow
-      .loadURL(
-        'data:text/html;charset=utf-8,' +
-        encodeURI(htmlString))
+    ejs.renderFile('./audio.ejs', templateData, (err, htmlString) => {
+      if (err) {
+        console.error(err)
+        htmlString = err
+      }
+      mainWindow
+        .loadURL(
+          'data:text/html;charset=utf-8,' +
+          encodeURI(htmlString))
+    })
   })
 })
 
---
+++ app/textToSpeech.py
@@ -0,0 +1,14 @@
+#API Key: RvjCX9UklZ_U67zgUclY9BAmkqStk9AwJVHPYoS8wrlk
+#URL: https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/4897dd9a-1109-4044-9739-b5236c8378d6
+import sys
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+text = sys.argv[1]
+
+authenticator = IAMAuthenticator('RvjCX9UklZ_U67zgUclY9BAmkqStk9AwJVHPYoS8wrlk')
+text_to_speech = TextToSpeechV1(authenticator=authenticator)
+text_to_speech.set_service_url('https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/4897dd9a-1109-4044-9739-b5236c8378d6')
+with open('audio_file.flac','wb') as audio_file:
+    audio_file.write(text_to_speech.synthesize(text, accept='audio/mp3').get_result().content)
+#print("test js->python integration")
